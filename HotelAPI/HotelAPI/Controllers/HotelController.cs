7e845f3ff4b1f8713595d4607a8b1cb828348e93
David Pérez <deividpa022@gmail.com>
2024-01-10 19:35:40 -0500

    Started building a hotel management API using .NET 7

---
+++ HotelAPI/HotelAPI/Controllers/HotelController.cs
@@ -0,0 +1,21 @@
+﻿using HotelAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        public IEnumerable<Hotel> GetHotels()
+        {
+            return new List<Hotel>
+            {
+                new Hotel{Id = 1, Name = "Hilton"},
+                new Hotel{Id = 2, Name = "Selene"},
+                new Hotel{Id = 3, Name = "Clock's"}
+            };
+        }
+    }
+}
---
+++ HotelAPI/HotelAPI/Models/Hotel.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelAPI.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
