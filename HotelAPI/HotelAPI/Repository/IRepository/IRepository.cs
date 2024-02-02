cc7c12ff928e6215f72349b4adbeba5a2ada3100
David Pérez <deividpa022@gmail.com>
2024-02-01 21:35:44 -0500

    IRepository and Repository files created to generalize the CRUD methods using Generycs

---
+++ HotelAPI/HotelAPI/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace HotelAPI.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task Create(T entity);
+        Task<List<T>> GetAll(Expression<Func<T, bool>>? filter = null);
+        Task<T> GetOne(Expression<Func<T, bool>>? filter = null, bool tracked=true);
+        Task Remove(T entity);
+        Task Save();
+
+    }
+}
---
+++ HotelAPI/HotelAPI/Repository/Repository.cs
@@ -0,0 +1,63 @@
+﻿using HotelAPI.Data;
+using HotelAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HotelAPI.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db) {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public async Task Create(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await Save();
+        }
+
+        public Task<List<T>> GetAll(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return query.ToListAsync();
+        }
+
+        public async Task<T> GetOne(Expression<Func<T, bool>>? filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task Remove(T entity)
+        {
+            dbSet.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
